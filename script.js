ff84395aefb5c885036336dc2bed8be118782092
kyleconkright <kyleconkright@gmail.com>
2016-04-11 13:51:32 -0700

    automatically get width and margin for left shift

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 $('document').ready(() => {
 
 	let addBio = function(thumb, target) {
+
+		let targetWidth = $('#content').parent().width();
+		let boxWidthPx = $('#content li').width();
+		let marginPx = $('#content li').css('margin').split('px');
+		let boxMargin = parseInt(100 * marginPx[0]) / targetWidth * 2;
+		let boxWidth = parseInt(100 * (boxWidthPx / targetWidth) + boxMargin ) + '%';
+
+		
 		$(thumb).on('click', (e) => {
 			var txt = '<li>'+$(e.target).html()+'</li>';
 			
 			$(target).prepend(txt);
-			$(target).css({'left':'-33.333333%'}).animate({'left':'0'}, 'easeOut', function(){
+			$(target).css({'left':'-'+boxWidth}).animate({'left':'0'}, () => {
 				$(target+' li').last().remove();
 			});
 		});
 	}
+
 
 	addBio('#content li', '#target');
 
