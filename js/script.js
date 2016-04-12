0ed0c3d3e2702102ae5aae2be427d47cfd16bcbd
kyleconkright <kyleconkright@gmail.com>
2016-04-11 20:14:46 -0700

    style

--- js/script.js
+++ js/script.js
@@ -4,4 +4,21 @@ $('document').ready(() => {
 		//direction: 'rtl'
 	});
 
+
+	let bigFade = function(a) {
+		$(a).css({'opacity':'0','position':'relative','top':'-16px'}).delay(100).animate({'opacity':'1', 'top':'0'});
+	}
+
+	bigFade('h1');
+	bigFade('header p');
+
+	let boxes = ($('#target li'));
+
+	for(let box of boxes) {
+		bigFade(box);
+	}
+
+
+	
+
 });
